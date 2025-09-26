301b31556d119802ed218fcea1d7d0f8bc45091b
dshomebg <125219067+dshomebg@users.noreply.github.com>
2025-09-26 21:20:05 +0300

    Reorder parameters in User model's username field

--- app/models.py
+++ app/models.py
@@ -4,6 +4,6 @@ from app.db import Base
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, index=True, nullable=False)
+    username = Column(String(50), unique=True, nullable=False, index=True)
     hashed_password = Column(String(128), nullable=False)
     is_admin = Column(Boolean, default=False)
