99cb67c416a2686cc72158c458715710de0e2c8d
dshomebg <125219067+dshomebg@users.noreply.github.com>
2025-09-27 10:04:37 +0300

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,17 +1,26 @@
 from pydantic import BaseModel
 
-class UserCreate(BaseModel):
-    username: str
-    password: str
-
 class UserOut(BaseModel):
     id: int
     username: str
     is_admin: bool
-
     class Config:
         orm_mode = True
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
+class TrafficBase(BaseModel):
+    year: int
+    month: int
+    organic: int = 0
+    brand: int = 0
+    facebook: int = 0
+    facebook_paid: int = 0
+    google_paid: int = 0
+    other: int = 0
+
+class TrafficCreate(TrafficBase):
+    pass
+
+class TrafficOut(TrafficBase):
+    id: int
+    class Config:
+        orm_mode = True
