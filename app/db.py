efa1f36988b9339bc7746acfa72bb02d7051cffb
dshomebg <125219067+dshomebg@users.noreply.github.com>
2025-09-27 10:44:36 +0300

    Fix DATABASE_URL formatting in db.py

--- app/db.py
+++ app/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://pagagal_dbuser:1Borabora@#@localhost/pagagal_dshome?charset=utf8mb4"
+DATABASE_URL = "mysql+pymysql://pagagal_dshome:1Borabora@#@localhost/pagagal_dshome?charset=utf8mb4"
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
