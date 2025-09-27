0eddf92520727a86b4fba955e6ff3b880e853641
dshomebg <125219067+dshomebg@users.noreply.github.com>
2025-09-27 10:04:23 +0300

    Modify User model and add Traffic model
    Updated User model to use Integer for fields and added Traffic model.

--- app/models.py
+++ app/models.py
@@ -1,9 +1,21 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer
 from app.db import Base
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, nullable=False, index=True)
-    hashed_password = Column(String(128), nullable=False)
-    is_admin = Column(Boolean, default=False)
+    username = Column(Integer, unique=True, index=True)
+    hashed_password = Column(Integer)
+    is_admin = Column(Integer)
+
+class Traffic(Base):
+    __tablename__ = "traffic"
+    id = Column(Integer, primary_key=True, index=True)
+    year = Column(Integer, nullable=False)
+    month = Column(Integer, nullable=False)
+    organic = Column(Integer, default=0)
+    brand = Column(Integer, default=0)
+    facebook = Column(Integer, default=0)
+    facebook_paid = Column(Integer, default=0)
+    google_paid = Column(Integer, default=0)
+    other = Column(Integer, default=0)
